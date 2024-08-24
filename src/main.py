b3ebbf18a11d43e6c1ec32beccf8bc3460033a2e
Felipe-Camarano <felipecamarano@gmail.com>
2024-08-23 21:10:30 -0300

    Alteração para ter dois carros disputando uma corrida de 600km.

--- src/main.py
+++ src/main.py
@@ -26,19 +26,24 @@ if __name__ == "__main__":
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input("Informe o nivel do tanque: "))
+    cm = float(input("Consumo Medio: "))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
 
     print('Cadastre o carro 2')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    litros = float(input("Informe o nivel do tanque: "))
+    cm = float(input("Consumo Medio: "))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, litros, cm)
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600:
+    while carro1.odometro < 600 and carro2.odometro < 600 and \
+            (carro1.tanque > 0 or carro2.tanque > 0):
         try:
             op_carro = 0
             while op_carro not in (1,2):
